e9436b31355a7149b827d897f41b8499107d8cf2
John Katsnelson <voltalocke@gmail.com>
2013-08-02 11:05:42 -0700

    attempted simple cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures express-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends         "nodejs", "= 1.1.2"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "nodejs"
+
+# Default yum package does not add node to the traditional loc on CentOS
+link "/usr/bin/node" do
+  to "/usr/local/bin/node"
+end
+
+# Install the NPM packages for the app
+execute "npm_install" do
+    command "cd #{node['app_root']} && /usr/local/bin/npm install"
+end
+
+# Copy over Node Upstart config
+template "/etc/init/node.conf" do
+    source "node.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+end
+
+# Start the Node service defined in the conf above
+service "node" do
+    provider Chef::Provider::Service::Upstart
+    action :restart
+end
